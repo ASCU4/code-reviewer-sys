ae60b07691a0f260a50b99bf6d80781c78d8e362
Dhruv Sharma <dhruv6september@gmail.com>
2026-07-16 14:50:29 +0530

    config.py updated with PostGreSQL pass and user

--- Backend/config.py
+++ Backend/config.py
@@ -6,7 +6,7 @@ load_dotenv()
 class Config:
     SECRET_KEY=os.getenv("SECRET_KEY")
     DATABASE_URL=os.getenv(
-        "DATABASE_URL"
+        "DATABASE_URL", "postgresql+psycopg2://postgres:Viuv@localhost:5432/code_review_db"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS= False
     GEMINI_API_KEY=os.getenv("GEMINI_API_KEY")
